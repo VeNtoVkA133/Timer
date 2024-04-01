64017ec4797d6e2342b0aa24f9643524a89c5c07
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2024-04-01 10:34:05 +0300

    refactoring code

--- Timer.js
+++ Timer.js
@@ -3,9 +3,9 @@
 //Создай переменную в которой лежит строка вывода
 
 let btn = document.querySelector(`#start`);
-let timeinput = document.querySelector(`.time`);
-let timeoutput = document.querySelector(`outputtext`);
-let funcjobtimer; 
+let timeinput = document.querySelectorAll(`.time`);
+let timeoutput = document.querySelector(`#outputtext`);
+let funcjobtimer;
 let stop = false; // переменная которая выключает таймер
 
 let h;
@@ -15,38 +15,40 @@ let s;
 btn.addEventListener("click", startTimer); // Если в переменной btn добавиться событие "клик", то выполни функцию "startTimer".
 
 
-function startTimer(){
+function startTimer() {
 
     //добавь в переменную то, что лежит  в пункте массива входных данных
-h = timeinput[0].value;  
-m = timeoutput[1].value;
-s = timeinput[2].value;
+    h = timeinput[0].value;
+    m = timeinput[1].value;
+    s = timeinput[2].value;
 
 
 
-funcjobtimer = setInterval(changeTimer,1000 /**милисекунд*/ ) // переменная для включения (изменения времени в таймере) таймера с интервалом в 1 секунду
+    funcjobtimer = setInterval(changeTimer, 1000 /**милисекунд*/ ); // переменная для включения (изменения времени в таймере) таймера с интервалом в 1 секунду
 
 }
 
-function changeTimer(){ //Функция измени время в таймере
-    
+function changeTimer() { //Функция измени время в таймере
+
     // Убери одну секунду, в переменную выключения запиши значение, если всё == 0, то выключи, если нет, то нет.
     // Если переменная изменилать на выключи, то убери интервал из переменной, дабы таймер перестал работать
     s--
-    stop = checkTimer(h,m,s);
+    stop = checkTimer(h, m, s);
     if (stop == true) {
         clearInterval(funcjobtimer);
-    }else{
-        cahangeText(h,m,s);
+    } else {
+        changeText(h, m, s);
     }
 }
 
-function cahangeText(){
-    timeoutput = `${h} : ${m} :  ${s}`; // Найди переменные из тела и перенеси их в поле вывода
+function changeText() {
+    timeoutput.innerHTML = `${h} : ${m} :  ${s}`; // Найди переменные из тела и перенеси их в поле вывода
 }
 
 function checkTimer() {
-    if (h == 0 && m == 0&&s==0){
+    if (h == 0 && m == 0 && s == 0) {
         return true;
-    }else{return false} //Можно ли убрать полность else, по сути же если не истина, то не входи, а значит ничего не делай
+    } else {
+        return false;
+    } //Можно ли убрать полность else, по сути же если не истина, то не входи, а значит ничего не делай
 }
