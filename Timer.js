8cb84b9aac370c0ea3304d7085ab9a453dc68e61
VeNtoVkA133 <svyatoslav.stolyarov.s.sss2000@mail.ru>
2024-04-03 00:20:31 +0300

    + день и год, + начало поправки на високосный год
    Проверил и исправил неработоспособность таймера.
    Добавил день и год, так же сделал все поля работоспособными.
    Заложил начала поправки на високосный год.
    Осталось внедрить в таймер поправку, для этого нужно взять дату с пк

--- Timer.js
+++ Timer.js
@@ -3,50 +3,101 @@
 //Создай переменную в которой лежит строка вывода
 
 let btn = document.querySelector(`#start`);
-let timeinput = document.querySelector(`.time`);
-let timeoutput = document.querySelector(`outputtext`);
-let funcjobtimer; 
+let timeinput = document.querySelectorAll(`.time`);
+let timeoutput = document.querySelector(`#outputtext`);
+let timerInterval;
 let stop = false; // переменная которая выключает таймер
 
+let y;
+let d;
 let h;
 let m;
 let s;
 
-btn.addEventListener("click", startTimer); // Если в переменной btn добавиться событие "клик", то выполни функцию "startTimer".
+let leapyear;
 
-
-function startTimer(){
-
-    //добавь в переменную то, что лежит  в пункте массива входных данных
-h = timeinput[0].value;  
-m = timeoutput[1].value;
-s = timeinput[2].value;
-
-
-
-funcjobtimer = setInterval(changeTimer,1000 /**милисекунд*/ ) // переменная для включения (изменения времени в таймере) таймера с интервалом в 1 секунду
-
-}
-
-function changeTimer(){ //Функция измени время в таймере
-    
-    // Убери одну секунду, в переменную выключения запиши значение, если всё == 0, то выключи, если нет, то нет.
-    // Если переменная изменилать на выключи, то убери интервал из переменной, дабы таймер перестал работать
-    s--
-    stop = checkTimer(h,m,s);
-    if (stop == true) {
-        clearInterval(funcjobtimer);
-    }else{
-        cahangeText(h,m,s);
+btn.addEventListener("click", checkStartTimer); // Если в переменной btn добавиться событие "клик", то выполни функцию "startTimer".
+function checkStartTimer() {
+    if (timerInterval >= 1) {
+        clearInterval(timerInterval);
+        startTimer();
+    } else {
+        startTimer();
     }
 }
 
-function cahangeText(){
-    timeoutput = `${h} : ${m} :  ${s}`; // Найди переменные из тела и перенеси их в поле вывода
+function startTimer() {
+
+    //добавь в переменную то, что лежит  в пункте массива входных данных
+
+    y = timeinput[0].value || 0;
+    d = timeinput[1].value || 0;
+    h = timeinput[2].value || 0;
+    m = timeinput[3].value || 0;
+    s = timeinput[4].value || 0;
+
+
+
+    timerInterval = setInterval(changeTimer, 1000); // переменная для включения (изменения времени в таймере) таймера с интервалом в 1 секунду
+
 }
 
-function checkTimer() {
-    if (h == 0 && m == 0&&s==0){
+function changeTimer() { //Функция измени время в таймере
+
+    // Убери одну секунду, в переменную выключения запиши значение, если всё == 0, то выключи, если нет, то нет.
+    // Если переменная изменилать на выключи, то убери интервал из переменной, дабы таймер перестал работать
+
+    let year = +prompt('Year')
+    if (year % 400 == 0){
+        leapyear = true;
+    }else if (year % 100 != 0 ) {
+        leapyear = true;
+    }else if (year % 4){
+        leapyear = true;
+    }else{
+        leapyear = false;
+    } // true = значит год високосный; false = значит год не високосный
+
+
+
+    if (s == 1 && m > 0) {
+        s = 60;
+        m--
+    }else if (s == 1 && m == 0 && h > 0) {
+        m = 59;
+        s = 60;
+        h--
+    }else if (s == 1 && m == 0 && h == 0 && d > 0) {
+        h = 23;
+        m = 59;
+        s = 60;
+        d--
+    }else if (s == 1 && m == 0 && h == 0 &&d == 0 && y > 0) {
+        d = 364;
+        h = 23;
+        m = 59;
+        s = 60;
+        y--
+    }
+
+    s--
+    stop = checkTimer(y, d, h, m, s);
+    if (stop == true) {
+        clearInterval(timerInterval);
+        changeText(y, d, h, m, s);
+    } else {
+        changeText(y, d, h, m, s);
+    }
+}
+
+function changeText(y, d, h, m, s) {
+    timeoutput.innerHTML = `${y} : ${d} : ${h} : ${m} :  ${s}`; // Найди переменные из тела и перенеси их в поле вывода
+}
+
+function checkTimer(y, d, h, m, s) {
+    if (y == 0 && d == 0 && h == 0 && m == 0 && s == 0) {
         return true;
-    }else{return false} //Можно ли убрать полность else, по сути же если не истина, то не входи, а значит ничего не делай
+    } else {
+        return false;
+    } //Можно ли убрать полность else, по сути же если не истина, то не входи, а значит ничего не делай
 }
